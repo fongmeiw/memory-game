189cba6b43c7d8f47d1984a92b029b0638f4ac4e
Mei Wong <meiwong@Mac.lan>
2025-07-03 14:58:30 -0500

    fixed major bugs

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,52 @@ import MemoryGame from "./components/MemoryGame";
 
 const images = [
   {
-    "src": "https://images.unsplash.com/photo-1626808642875-0aa545482dfb",
+    "src": "https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?q=80&w=794&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "flipped": false,
     "match": false
   },
   {
-      "src": "https://images.unsplash.com/photo-1546842931-886c185b4c8c",
+      "src": "https://images.unsplash.com/photo-1669707355372-b2d1e31dc083?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "flipped": false,
       "match": false
   },
   {
-      "src": "https://images.unsplash.com/photo-1520763185298-1b434c919102",
+      "src": "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=776&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "flipped": false,
       "match": false
   },
   {
-      "src": "https://images.unsplash.com/photo-1442458017215-285b83f65851",
+      "src": "https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=830&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "flipped": false,
       "match": false
   },
   {
-      "src": "https://images.unsplash.com/photo-1496483648148-47c686dc86a8",
+      "src": "https://images.unsplash.com/photo-1585533530535-2f4236949d08?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "flipped": false,
       "match": false
   },
   {
-      "src": "https://images.unsplash.com/photo-1591181520189-abcb0735c65d",
+      "src": "https://images.unsplash.com/photo-1615824996195-f780bba7cfab?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "flipped": false,
+      "match": false
+  },
+  {
+      "src": "https://plus.unsplash.com/premium_photo-1661890071978-6c80f92c7fdf?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "flipped": false,
+      "match": false
+  },
+  {
+      "src": "https://images.unsplash.com/photo-1574870111867-089730e5a72b?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "flipped": false,
+      "match": false
+  },
+  {
+      "src": "https://images.unsplash.com/photo-1535083783855-76ae62b2914e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "flipped": false,
       "match": false
   }
+
+  
 ]
 function App() {
   return (
--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
-import { shuffle } from 'lodash';
+import { useEffect, useState, useRef } from "react";
+import { matches, shuffle } from 'lodash';
 import './MemoryGame.css'; 
 
 type ImageCard = {
   src: string;
-  match: Boolean;
-  flipped: Boolean;
+  match: boolean;
+  flipped: boolean;
   id?: string; // ID for each image card
 }
 type MemoryGameProps = {
@@ -14,6 +14,11 @@ type MemoryGameProps = {
 
 const MemoryGame = ({ images }: MemoryGameProps) => {
   const [placeImages, setPlaceImages] = useState<ImageCard[]>([]);
+  const [ timer, setTimer ] = useState<number>(0);
+  const intervalRef = useRef<number | null>(null);
+  // const [ duration, setDuration ] = useState<number>(0)
+  const [ isRunning, setRunning ] = useState<boolean>(false);
+  const [ isBlocked, setBlock ] = useState<boolean>(false);
 
   // Initialize the game by duplicating and shuffling the images
   // This will create a set of pairs for the memory game
@@ -23,10 +28,28 @@ const MemoryGame = ({ images }: MemoryGameProps) => {
   // The shuffled images are then set to the placeImages state
 
   useEffect(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    };
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTimer(prevTimer => prevTimer + 1)
+      }, 1000)
+    }
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    }
+  }, [isRunning]);
+
+  useEffect(() => {
     const duplicateImages = [...images, ...images].map((image, index) => ({
       ...image,
       id: image.src + index, // Assign a unique ID based on the index
     }));
+
     const shuffledImages = shuffle(duplicateImages);
     setPlaceImages(shuffledImages);
   }, []);
@@ -35,12 +58,16 @@ const MemoryGame = ({ images }: MemoryGameProps) => {
     // Check if the two flipped images match
     if (first.src === second.src) {
       // If they match, keep them flipped and mark as matched
-      setPlaceImages((prevImages) =>
-        prevImages.map((image) =>
+      setPlaceImages((prevImages) => {
+        const matchedImages = prevImages.map((image) =>
           image.id === first.id || image.id === second.id
             ? { ...image, match: true } // Mark as matched
             : image)
-      );
+        if (matchedImages.filter(img => !img.match).length === 0) {
+          setRunning(false);
+        }
+        return matchedImages;
+    });
     } else {
       // If they don't match, flip them back after a short delay
       console.log("Images do not match, flipping back after delay");
@@ -52,38 +79,65 @@ const MemoryGame = ({ images }: MemoryGameProps) => {
               : image
           )
         );
-      }, 1000);
+      }, 300);
     }
   };
 
   const handleClick = (id: string) => {
-    const card = placeImages.find((image) => image.id === id);
-    if (card?.flipped) return; // Ignore if already flipped
+    if (!isBlocked) {
+      if (placeImages.filter(img => img.flipped).length === 0) {
+        setRunning(true)
+      }
+      const card = placeImages.find((image) => image.id === id);
+      if (card?.flipped) return; // Ignore if already flipped
 
-    // Update the flipped state of the clicked image
-    const updatedImages = placeImages.map((image) =>
-      image.id === id ? { ...image, flipped: true } : image
-    );
+      // Update the flipped state of the clicked image
+      const updatedImages = placeImages.map((image) =>
+        image.id === id ? { ...image, flipped: true } : image
+      );
 
-    setPlaceImages(updatedImages);
+      setPlaceImages(updatedImages);
 
-    const justFlipped = updatedImages.filter(img => img.flipped && !img.match);
-    if (justFlipped.length === 2) {
-      // Check if the two flipped images match
-      const [first, second] = justFlipped;
-      
+      const justFlipped = updatedImages.filter(img => img.flipped && !img.match);
+      if (justFlipped.length === 2) {
+        // Check if the two flipped images match, blocking mouse click
+        setBlock(true);
+        const [first, second] = justFlipped;
+        
 
-      setTimeout(() => {
-        checkMatch(first, second);
-      }, 500); // Delay to allow user to see the flipped images
+        setTimeout(() => {
+          checkMatch(first, second);
+          setBlock(false);
+        }, 600); // Delay to allow user to see the flipped images
+      }
     }
   };
 
+  const startTheGame = () => {
+    setRunning(true);
+  }
+
+  const handleReset = () => {
+    setPlaceImages((prevImages) => {
+      const resetImages = prevImages.map(image => {
+        return {...image, match: false, flipped: false} 
+      });
+
+      return shuffle(resetImages);
+    });
+    setTimer(0); // ← this resets the counter
+    setRunning(false);
+  }
+
   return (
-    <div>
-      <h1>Memory Game</h1>
-      <p>Build your memory game! </p>
-      <p>Here are the sample images:</p>
+    <div className={isBlocked ? "block" : ""}>
+      <div style={{ textAlign: "center" }}>
+        <h1>Memory Game</h1>
+        <div style={{display: "flex", justifyContent: "center", gap: "8px"}}>
+          <button onClick={handleReset}>Reset</button><button onClick={startTheGame}>Start</button>
+        </div>
+        <div style={{marginBottom: "8px"}}><label>Timer: {timer} s</label></div>
+      </div>
       <div className="gallery">
       {placeImages.map((image) => (
         image.flipped ? <img
